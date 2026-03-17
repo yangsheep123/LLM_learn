7e5cd8a07a6c2ecf4f573021722b151a7c997b63
yangsheep123 <mianhuatangit@gmail.com>
2026-03-17 20:12:34 +0800

    Chapter 9

---
+++ 第九章/批量归一化.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader,Dataset
+
+class MyModel(nn.Module):
+    def __init__(self):
+        self.model = nn.Sequential(
+            nn.Linear(28*28,128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(128,128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(128,128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(128,64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            
+            nn.Linear(64,10)
+        )
