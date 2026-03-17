8d584464b62db8be7652dc028653e58f8b18b4b5
yangsheep123 <mianhuatangit@gmail.com>
2026-03-17 20:22:43 +0800

    修正：批量归一化时，bias应该为false

--- 第九章/批量归一化.py
+++ 第九章/批量归一化.py
@@ -5,25 +5,22 @@ from torch.utils.data import DataLoader,Dataset
 class MyModel(nn.Module):
     def __init__(self):
         self.model = nn.Sequential(
-            nn.Linear(28*28,128),
+            nn.Linear(28*28,128,bias=False),
             nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Dropout(0.5),
 
-            nn.Linear(128,128),
+
+            nn.Linear(128,128,bias=False),
             nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Dropout(0.5),
 
-            nn.Linear(128,128),
+            nn.Linear(128,128,bias=False),
             nn.BatchNorm1d(128),
             nn.ReLU(),
-            nn.Dropout(0.5),
 
-            nn.Linear(128,64),
+            nn.Linear(128,64,bias=False),
             nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            
+
             nn.Linear(64,10)
         )
